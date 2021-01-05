1322e09b501cdfd186da7b1ebd839502711fe9f2
slava bily <slavabily@me.com>
2021-01-05 10:47:41 +0200

    Initial Commit

---
+++ FireCardsRep/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FireCardsRep
+//
+//  Created by slava bily on 05.01.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
